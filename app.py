a3281d47fc43ddf8f4a1ac381b6887cee5b3fb58
marius-george <>
2023-08-02 10:38:48 +0000

    Create templates and user folder. Created files in templates and user folder. Built database schema

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from blogproject import app
+from blogproject import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- blogproject/__init__.py
+++ blogproject/__init__.py
@@ -1,10 +1,38 @@
-# blogproject __init__.py
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
-app = Flask(__name__)
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
 
 
-from blogproject.core.views import core
-from blogproject.custom_error.errors import custom_error
-app.register_blueprint(core)
-app.register_blueprint(custom_error)
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'secretblog'
+
+    # Database setup #
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+        os.path.join(basedir, 'data.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Login configs
+    login_manager.init_app(app)
+    login_manager.login_view = 'users.login'
+
+    from blogproject.core.views import core
+    from blogproject.users.views import users
+    from blogproject.custom_error.errors import custom_error
+
+    app.register_blueprint(core)
+    app.register_blueprint(users)
+    app.register_blueprint(custom_error)
+
+    return app
