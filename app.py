2ca2586bb8b704161dc60d283e598cc7ede1e3b4
marius-george <>
2023-09-29 20:04:25 +0000

    Unable to deploy

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
 from blogproject import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
+    
--- blogproject/__init__.py
+++ blogproject/__init__.py
@@ -4,29 +4,27 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+if os.path.exists("env.py"):
+    import env
+
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 
-
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = 'secretblog'
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-    # Database setup #
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-        os.path.join(basedir, 'data.sqlite')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    if os.environ.get("DEVELOPMENT") == "True":
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+    else:
+        uri = os.environ.get("DATABASE_URL")
+        if uri.startswith("postgres://"):
+            uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-    db.init_app(app)
-    migrate.init_app(app, db)
-
-    # Login configs
-    login_manager.init_app(app)
-    login_manager.login_view = 'users.login'
-
+    # Import and register blueprints
     from blogproject.core.views import core
     from blogproject.users.views import users
     from blogproject.blog_posts.views import blog_posts
