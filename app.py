7a1ae328e7b96e2bb2ba2f3c44dcbe6d48a237d4
marius-george <>
2023-10-10 20:07:50 +0000

    Changed database setup

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
 from blogproject import create_app
 
+try:
+    import env
+except:
+    pass
 app = create_app()
 
 if __name__ == '__main__':
     app.run(
         host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
+        port=int(os.environ.get("PORT", "5000")),
         debug=os.environ.get("DEBUG")
-    )
-    
+    )     
--- blogproject/__init__.py
+++ blogproject/__init__.py
@@ -4,27 +4,42 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-if os.path.exists("env.py"):
-    import env
-
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 
+
 def create_app():
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    app.config['SECRET_KEY'] = 'secretblog'
+
+    # Database setup #
+    # basedir = os.path.abspath(os.path.dirname(__file__))
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    #     os.path.join(basedir, 'data.sqlite')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+    # app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+    # app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
     if os.environ.get("DEVELOPMENT") == "True":
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
     else:
         uri = os.environ.get("DATABASE_URL")
+        print("uri", uri)
         if uri.startswith("postgres://"):
             uri = uri.replace("postgres://", "postgresql://", 1)
-        app.config["SQLALCHEMY_DATABASE_URI"] = uri
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
-    # Import and register blueprints
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Login configs
+    login_manager.init_app(app)
+    login_manager.login_view = 'users.login'
+
     from blogproject.core.views import core
     from blogproject.users.views import users
     from blogproject.blog_posts.views import blog_posts
