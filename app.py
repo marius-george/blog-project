30741313d5f5dcc3f4c3f71b9c489edba12cd3a8
marius-george <>
2023-10-12 22:24:02 +0000

    Modified code for deployment

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import os
 from blogproject import create_app
 
-try:
-    import env
-except:
-    pass
 app = create_app()
 
 if __name__ == '__main__':
     app.run(
         host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT", "5000")),
+        port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
-    )     
+    )
